ae064feb0ba743e0e2e18f79009b55c1b136b009
Matheus Andrade <matheus.andrade.ext@conductor.com.br>
2019-02-02 19:35:00 -0200

    fisrt commit

---
+++ CustomerManagerSystem/CustomerManagerSystem.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomerManagerSystem.WEB.MvcApplication" Language="C#" %>
---
+++ CustomerManagerSystem/CustomerManagerSystem.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerManagerSystem.WEB.Startup))]
+namespace CustomerManagerSystem.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
